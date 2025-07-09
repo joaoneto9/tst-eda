262ccd80ee9746475e2dcaa03b0b47edc7d5b490
joaoneto9 <joaonetocg1@gmail.com>
2025-07-08 23:21:56 -0300

    MergeSorts Steps Algorithm

--- exercises/MelhorPivot.java
+++ exercises/MelhorPivot.java
@@ -7,12 +7,11 @@ class MelhorPivot {
 		Scanner sc = new Scanner(System.in);
 
 		int[] entrada = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[] copy = entrada;
 
 		int[] indices = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 		int iDifPrimeiro = Math.abs((entrada.length / 2) - partiona(entrada, indices[0]));
-		int iDifSegundo = Math.abs((entrada.length / 2) - partiona(copy, indices[1]));
+		int iDifSegundo = Math.abs((entrada.length / 2) - partiona(entrada, indices[1]));
 
 		if (iDifPrimeiro > iDifSegundo)
 			System.out.println(indices[1]);
@@ -31,13 +30,10 @@ class MelhorPivot {
 		int j = entrada.length - 2;
 
 		for (int i = j; i >= 0; i--) {
-			if (entrada[i] >= entrada[entrada.length - 1]) {
-				swap(entrada, j, i);
+			if (entrada[i] >= entrada[entrada.length - 1])
 				j--; 
-			}
 		}
 
-		swap(entrada, j + 1, entrada.length - 1); // posiciona na posicao pivot
 		return j + 1; // retorna a posicao do pivot
 	}
 
